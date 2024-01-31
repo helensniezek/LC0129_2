fbb8f311f0c48da9a5ab56bd75ef2e14e753bc41
helensniezek <helensniezek@gmail.com>
2024-01-31 11:57:55 -0600

    Add project files.

---
+++ LC0129_2/Program.cs
@@ -0,0 +1,78 @@
+﻿// MIS 3013 001 
+// Jan 29, 2024
+// Helen Sniezek
+// 113552793
+
+// comments
+// 
+Console.WriteLine("MIS3013 Jan 29");
+// WriteLine: function, name, machine, coffee maker
+// WriteLine(): function call, coffee, 
+// 
+
+// CPU: calculation
+// RAM memory: store data
+
+// datatypes (resources)
+// simple: int double bool char, not expensive, not precious
+// complex: expensive resources. anything else is complex
+
+// two rules
+// 1. letters _ numbers
+// 2. letter _ (could not start with a number)
+
+int age1;// age1 variable, variable name, instance, object
+// age1: int, simple, not expensive
+// int is the room type. room key card 
+double w1;// double, w1 is the card;double simple not expensive
+// 0: 
+
+bool b1;// b1 bool simple, not expensive
+char c1;// cahr, simple, not expensive
+
+// = assignment oporator
+age1 = 20 + 2;// assignment
+// 1.  right hand: 20+2 expression, algebra, CPU
+// 2. store the value (put the value into space)
+
+age1 = age1 + 5;// assignment 
+// 1. age1 + 5
+// 2. 27
+
+// complex datatype
+// string: complex expensive
+string str1;// str1 string complex expensive
+// you do not get a string space
+str1 = "20";
+
+Console.WriteLine(str1);
+
+// datatype conversion
+age1 = Convert.ToInt32(str1);// ToInt32 assignment, 
+// ToInt32() function call
+// 99 
+// 00
+// 9999
+// int: 4 Bytes, 1 Bytes = 8 bits 32 bits
+
+// paramters, ingredients
+w1 = Convert.ToDouble(str1);// ToDouble function name assignment
+// ToDouble() function call
+// 
+
+//
+str1 = Console.ReadLine();// ReadLine name
+// ReadLine();// 
+
+Console.WriteLine(str1);
+
+Console.Write("Please input an age: ");// Write
+// Write()
+
+str1 = Console.ReadLine();// ReadLine
+
+age1 = Convert.ToInt32(str1);// 
+
+age1 = age1 + 10;// assignment 
+
+Console.WriteLine(age1);
